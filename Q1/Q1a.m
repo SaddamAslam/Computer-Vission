9883d492408bb738e10fc4fa274ccc69435e4716
SaddamAslam <38394943+SaddamAslam@users.noreply.github.com>
2019-02-22 19:30:41 +0500

    Add files via upload
    University Of management and technology computer vision syed farooq ali Assignment 4 Q1

---
+++ Q1/Q1a.m
@@ -0,0 +1,73 @@
+file = importdata('Q1a.txt');
+dat = file.data;
+im = imread('mecca06.pgm');
+[row,col] = size(im);
+Shear = zeros(3,3);
+Rot = zeros(3,3);
+Rot(1,1) = dat(2,1);
+Rot(1,2) = dat(3,1);
+Rot(1,3) = dat(4,1);
+Rot(2,1) = dat(5,1);
+Rot(2,2) = dat(6,1);
+Rot(2,3) = dat(7,1);
+Rot(3,1) = dat(8,1);
+Rot(3,2) = dat(9,1);
+Rot(3,3) = dat(10,1);
+Shear(1,1) = dat(11,1);
+Shear(1,2) = dat(12,1);
+Shear(1,3) = dat(13,1);
+Shear(2,1) = dat(14,1);
+Shear(2,2) = dat(15,1);
+Shear(2,3) = dat(16,1);
+Shear(3,1) = dat(17,1);
+Shear(3,2) = dat(18,1);
+Shear(3,3) = dat(19,1);
+Trans=Shear*Rot;
+iTrans= inv(Trans);
+co = zeros(3,1);
+ for i=1: row
+     for j=1: col
+           co(1,1) = i;
+           co(2,1) = j;
+           co(3,1) = 1;
+           newco = iTrans*co;
+           x = newco(1,1);
+           y = newco(2,1);
+           xlb = floor(x);
+           ylb = floor(y);
+           if(xlb <= 0)
+             xlb = 1;
+           end
+           if(xlb > row)
+             xlb = row;
+           end
+           if(ylb <= 0)
+             ylb = 1;
+           end
+           if(ylb > col)
+             ylb = col;
+           end
+           xub = xlb+1;
+           yub = ylb+1;
+           if(xub <= 0)
+              xub = 1;
+           end
+          if(xub > row)
+             xub = row;
+          end
+          if(yub <= 0)
+             yub = 1;
+          end
+          if(yub > col)
+             yub = col;
+          end
+          exub = xub-x;
+          eyub = yub-y;
+          exlb = x-xlb;
+          eylb = y-ylb;
+          imOut(i,j) = (exub*eyub*im(xlb,ylb))+(exlb*eyub*im(xub,ylb))+(exub*eylb*im(xlb,yub))+(exlb*eylb*im(xub,yub));
+      end
+ end
+ 
+figure ,imshow(imOut)
+imwrite(imOut,'mecca06_a.pgm');
---
+++ Q1/Q1c.m
@@ -0,0 +1,63 @@
+file = importdata('Q1c.txt');
+dat = file.data;
+im = imread('mecca06.pgm');
+[row,col] = size(im);
+Scaling = zeros(3,3);
+Scaling(1,1) = dat(2,1);
+Scaling(1,2) = dat(3,1);
+Scaling(1,3) = dat(4,1);
+Scaling(2,1) = dat(5,1);
+Scaling(2,2) = dat(6,1);
+Scaling(2,3) = dat(7,1);
+Scaling(3,1) = dat(8,1);
+Scaling(3,2) = dat(9,1);
+Scaling(3,3) = dat(10,1);
+Trans=Scaling;
+iTrans= inv(Trans);
+co = zeros(3,1);
+ for i=1: row
+     for j=1: col
+           co(1,1) = i;
+           co(2,1) = j;
+           co(3,1) = 1;
+           newco = iTrans*co;
+           x = newco(1,1);
+           y = newco(2,1);
+           xlb = floor(x);
+           ylb = floor(y);
+           if(xlb <= 0)
+             xlb = 1;
+           end
+           if(xlb > row)
+             xlb = row;
+           end
+           if(ylb <= 0)
+             ylb = 1;
+           end
+           if(ylb > col)
+             ylb = col;
+           end
+           xub = xlb+1;
+           yub = ylb+1;
+           if(xub <= 0)
+              xub = 1;
+           end
+          if(xub > row)
+             xub = row;
+          end
+          if(yub <= 0)
+             yub = 1;
+          end
+          if(yub > col)
+             yub = col;
+          end
+          exub = xub-x;
+          eyub = yub-y;
+          exlb = x-xlb;
+          eylb = y-ylb;
+          imOut(i,j) = (exub*eyub*im(xlb,ylb))+(exlb*eyub*im(xub,ylb))+(exub*eylb*im(xlb,yub))+(exlb*eylb*im(xub,yub));
+      end
+ end
+ 
+figure ,imshow(imOut);
+imwrite(imOut,'mecca06_c.pgm');
